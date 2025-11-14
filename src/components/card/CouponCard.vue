8aa6ba3ae71fe7f973d64b9553eb6c0841056de3
Carlos Davi <carlosdavi004@gmail.com>
2025-11-14 10:48:32 -0300

    Teste do husky

--- src/components/card/CouponCard.vue
+++ src/components/card/CouponCard.vue
@@ -52,7 +52,10 @@ const getColorDate = computed((): string => {
 
   const day = parseInt(parts[0], 10);
   const month = parseInt(parts[1], 10);
-  const year = parseInt(parts[2], 10);
+
+
+
+  const year =                    parseInt(parts[2], 10);
 
   const dateProp = new Date(year, month - 1, day);
 
